43eefeea8d48edeca675bc980f9e2ac079d07f9f
cptuulia <tantonius@icloud.com>
2025-07-17 14:43:53 +0200

    Change step function

--- src/react/src/App.jsx
+++ src/react/src/App.jsx
@@ -1,18 +1,35 @@
 import { useState } from 'react'
+
+
 import './style/App_{{LANGUAGE_CODE}}.scss'
-
-
 import Header from "./components/Header/Header_{{LANGUAGE_CODE}}.jsx";
 import Step1 from "./steps/Step1_{{LANGUAGE_CODE}}.jsx";
 function App() {
-  const [currentStep] = useState(1)
+  const [currentStep, setCurrentStep] = useState(1)
+
+  //
+  // Change Step
+  //
+  const changeStep = (stepNUmber, e) => {
+    setCurrentStep(stepNUmber);
+  };
+
 
   return (
     <>
 
       <header> <Header /></header>
+
+
       <div id="mainContent">
-        { currentStep == 1 && <Step1 />}
+        {currentStep == 1 &&
+          <Step1
+            emitChangeStep={changeStep}
+          />}
+
+        {currentStep == 2 &&
+          <div>Step2</div>
+        }
       </div>
     </>
   )
--- src/react/src/steps/Step1.jsx
+++ src/react/src/steps/Step1.jsx
@@ -3,27 +3,41 @@
  * Step 1
  * 
  */
-import react from 'react'
+import PropTypes from 'prop-types';
+
 import { useState } from 'react';
 import './steps_{{LANGUAGE_CODE}}.scss'
+import './step1_{{LANGUAGE_CODE}}.scss'
 
 import yes from '/src/assets/img/yes_{{LANGUAGE_CODE}}.png';
-function Step1() {
+function Step1(props) {
 
+        //
+        // Variables to control the visibility of the sections
+        //
         const [tellMeMore, setTellMeMore] = useState(false);
         const [readMore1, setReadMore1] = useState(false);
         const [readMore2, setReadMore2] = useState(false);
 
+        //
+        // Update Tell More visibility
+        //
         const tellMore = (e) => {
                 let status = tellMeMore ? false : true;
                 setTellMeMore(status);
         };
 
-         const changeReadMore1 = (e) => {
+        //
+        // Update Read More 1 visibility
+        //
+        const changeReadMore1 = (e) => {
                 let status = readMore1 ? false : true;
                 setReadMore1(status);
         };
 
+        //
+        // Update Read More 2 visibility
+        //
         const changeReadMore2 = (e) => {
                 let status = readMore2 ? false : true;
                 setReadMore2(status);
@@ -38,7 +52,7 @@ function Step1() {
                                 [[step1.intro]]
                         </p>
 
-                         {/* Tell me more */}
+                        {/* Tell me more */}
                         {tellMeMore &&
                                 <div className='tellMeMore'>
 
@@ -51,66 +65,70 @@ function Step1() {
                                         <p>[[step1.optout1.p]]</p>
 
 
-                                                {/* Read more 1 */}
-                                                {!readMore1 &&
-                                                        <div className='readMore'>
-                                                                <div className='moreButton' onClick={changeReadMore1} >
-                                                                        [[button.readmore.more]]
-                                                                </div>
+                                        {/* Read more 1 */}
+                                        {!readMore1 &&
+                                                <div className='readMore'>
+                                                        <div className='moreButton' onClick={changeReadMore1} >
+                                                                [[button.readmore.more]]
                                                         </div>
+                                                </div>
 
-                                                }
-                                                {readMore1 &&
-                                                        <div className='readMore'>
-                                                                <div className='lessButton' onClick={changeReadMore1} >
-                                                                        [[button.readmore.less]]
-                                                                </div>
-                                                                <div className='clearBoth'></div>
-                                                                <p>[[step1.optout1.readmore]]</p>
+                                        }
+                                        {readMore1 &&
+                                                <div className='readMore'>
+                                                        <div className='lessButton' onClick={changeReadMore1} >
+                                                                [[button.readmore.less]]
                                                         </div>
-                                                }
+                                                        <div className='clearBoth'></div>
+                                                        <p>[[step1.optout1.readmore]]</p>
+                                                </div>
+                                        }
 
-                                         {/* Tell me more 2*/}
+                                        {/* Tell me more 2*/}
                                         <div className='introHeader'>
                                                 <img src={yes} className="checkBoxSlider" />
                                                 <h1 className='intro'>[[step1.optout2.h1]]</h1>
                                         </div>
                                         <div className='clearBoth'></div>
                                         <p>[[step1.optout2.p]]</p>
-                                        
-                                                {/* Read more 2 */}
-                                                {!readMore2 &&
-                                                        <div className='readMore'>
-                                                                <div className='moreButton' onClick={changeReadMore2} >
-                                                                        [[button.readmore.more]]
-                                                                </div>
-                                                        </div>
 
-                                                }
-                                                {readMore2 &&
-                                                        <div className='readMore'>
-                                                                <div className='lessButton' onClick={changeReadMore2} >
-                                                                        [[button.readmore.less]]
-                                                                </div>
-                                                                <div className='clearBoth'></div>
-                                                                <p>[[step1.optout2.readmore]]</p>sada
+                                        {/* Read more 2 */}
+                                        {!readMore2 &&
+                                                <div className='readMore'>
+                                                        <div className='moreButton' onClick={changeReadMore2} >
+                                                                [[button.readmore.more]]
                                                         </div>
-                                                }        
+                                                </div>
+
+                                        }
+                                        {readMore2 &&
+                                                <div className='readMore'>
+                                                        <div className='lessButton' onClick={changeReadMore2} >
+                                                                [[button.readmore.less]]
+                                                        </div>
+                                                        <div className='clearBoth'></div>
+                                                        <p>[[step1.optout2.readmore]]</p>sada
+                                                </div>
+                                        }
 
                                 </div>
                         }
 
-                        {/* Tell me more  and Opt Me Out button*/}
+                        {/* Tell me more  and Opt Me Out buttons */}
                         <div className='buttons'>
                                 {!tellMeMore &&
                                         <div className="tellMeMoreButton" onClick={tellMore}>
                                                 [[button.tellmemore]]
                                         </div>
                                 }
-                                <div class="buttonActive">[[button.OptMeOut]]</div>
+                                <div className="buttonActive"
+                                        onClick={(e) => props.emitChangeStep(2, e)}
+                                >
+                                        [[button.OptMeOut]]
+                                </div>
                         </div>
 
-                        {/* Please note */}
+                        {/* Please note section */}
                         <div className='clearBoth'></div>
                         {tellMeMore &&
                                 <p className='pleaseNote'>
@@ -123,4 +141,8 @@ function Step1() {
         )
 }
 
+
+Step1.propTypes = {
+        emitChangeStep: PropTypes.func,  // Change step
+};
 export default Step1
